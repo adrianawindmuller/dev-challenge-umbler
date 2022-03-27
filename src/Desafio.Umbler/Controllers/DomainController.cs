3b3cbe35efd427b0506d77ca3401794c4e32e5d1
Adriana Windmuller <adrianawindmuller@outlook.com>
2022-03-27 18:10:39 -0300

    the routine has been documented

--- src/Desafio.Umbler/Controllers/DomainController.cs
+++ src/Desafio.Umbler/Controllers/DomainController.cs
@@ -22,20 +22,26 @@ namespace Desafio.Umbler.Controllers
         [HttpGet, Route("domain/{domainName}")]
         public async Task<IActionResult> Get(string domainName)
         {
+            // 1. identifica se já existe um dominio com este nome no banco
             var domain = await _db.Domains.FirstOrDefaultAsync(d => d.Name == domainName);
 
+            // 2. se o dominio não existe vi criar um novo registro no banco
             if (domain == null)
             {
+                // 2.1 consulta informações de contato e os DNS do endereço via nome
                 var response = await WhoisClient.QueryAsync(domainName);
 
+                // 2.2 consulta os DNS
                 var lookup = new LookupClient();
                 var result = await lookup.QueryAsync(domainName, QueryType.ANY);
                 var record = result.Answers.ARecords().FirstOrDefault();
                 var address = record?.Address;
                 var ip = address?.ToString();
 
+                // 2.3 consulta novamente as informações de contato e os DNS do endereço via IP
                 var hostResponse = await WhoisClient.QueryAsync(ip);
 
+                // 2.4 monta a novo objeto de Domain
                 domain = new Domain
                 {
                     Name = domainName,
@@ -46,21 +52,27 @@ namespace Desafio.Umbler.Controllers
                     HostedAt = hostResponse.OrganizationName
                 };
 
+                // 2.5 add no bd o domain
                 _db.Domains.Add(domain);
             }
 
+            //  consulta se a data de atualização é maior que o Ttl, se não vai atualizar o registro no banco
             if (DateTime.Now.Subtract(domain.UpdatedAt).TotalMinutes > domain.Ttl)
             {
+                // consulta os dados de contato e DNS
                 var response = await WhoisClient.QueryAsync(domainName);
 
+                // consulta os DNs
                 var lookup = new LookupClient();
                 var result = await lookup.QueryAsync(domainName, QueryType.ANY);
                 var record = result.Answers.ARecords().FirstOrDefault();
                 var address = record?.Address;
                 var ip = address?.ToString();
 
+                // consulta novamente os dados de contato e DNS via IP
                 var hostResponse = await WhoisClient.QueryAsync(ip);
-
+                
+                // atualiza os dados de Domain
                 domain.Name = domainName;
                 domain.Ip = ip;
                 domain.UpdatedAt = DateTime.Now;
